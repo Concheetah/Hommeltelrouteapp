c8b77cbfbbd95a9a8b7914620a12d68096ca7000
Conchita <conchitamartin94@gmail.com>
2024-12-06 11:07:14 +0100

    app.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+import Amplify from "aws-amplify/core";
+import config from "./src/aws-exports";
+
+Amplify.configure(config);
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
