077bcffe52d4081bac676a57f802a40fef0e5d94
Conchita <conchitamartin94@gmail.com>
2024-12-26 13:54:05 +0100

    update eas

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import { View, Text } from "react-native";
 
-import Amplify from "aws-amplify/core";
-import config from "./src/aws-exports";
-
-Amplify.configure(config);
 
 const App = () => {
   return (
